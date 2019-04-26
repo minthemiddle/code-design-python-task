71671ced36786fc35a1504c7fcfcb922c7b9f60b
Moritz Eich <moritz.eich@joblift.de>
2019-04-26 17:07:00 +0200

    initial commit

---
+++ zahlenraten.py
@@ -0,0 +1,27 @@
+goal_number = 100
+
+ 
+def check_number(x):
+    if x == goal_number :
+        print("congratulations. You won, Your number is right")
+    elif goal_number < x:
+         print("the number is lower")
+    else:
+        print("the number is higher")
+
+
+programm_running = True
+         
+while(programm_running):
+    inp = input("guess the number: ")
+    inp = int(inp)
+    check_number(inp)
+         
+ '''        
+  Explain
+ 1. Explain game: Set a number that someone guess
+ 2. Define your 
+ 
+  Iteration
+ - "Your number is {}"
+'''
